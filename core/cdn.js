dfa281e1cf293aaedfb3d2ab790b3590b9315482
DashDash Studios <79600449+dashdash-studios@users.noreply.github.com>
2023-04-21 07:18:00 -0700

    Update cdn.js

--- core/cdn.js
+++ core/cdn.js
@@ -1,1 +1,4 @@
-
+var url = 'https://raw.githubusercontent.com/dashdash-studios/tw-airway/main/core/styles.css';
+var style;
+fetch(url).then(response => { response.text().then(text => { style = text }});
+console.log(style);
