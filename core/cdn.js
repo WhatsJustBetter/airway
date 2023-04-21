d2652242a9b03cf168927fe25e47c526f2bc5ea9
DashDash Studios <79600449+dashdash-studios@users.noreply.github.com>
2023-04-21 07:38:04 -0700

    fix

--- core/cdn.js
+++ core/cdn.js
@@ -1,4 +1,4 @@
-document.onready => {
+document.onready = function() {
   var style;
   fetch('https://raw.githubusercontent.com/dashdash-studios/tw-airway/main/core/styles.css').then(r=>{r.text().then(t=>{style=t})});
   console.log(style);
